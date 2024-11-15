c1e718d4b4421b7b66e4e930b568a5a9b70733aa
njuguna05 <njuguna05@gmail.com>
2024-11-15 19:30:57 +0300

    Update settings.py
    Signed-off-by: njuguna05 <njuguna05@gmail.com>

--- grant_app/settings.py
+++ grant_app/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("POSTGRES_DB", "grant_app_db"),
         'USER': os.getenv("POSTGRES_USER", "postgres"),
-        'PASSWORD': os.getenv("POSTGRES_PASSWORD", "samiek"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", "postgres"),
         'HOST': os.getenv("POSTGRES_HOST", "localhost"),
         'PORT': os.getenv("POSTGRES_PORT", "5432"),
     }
